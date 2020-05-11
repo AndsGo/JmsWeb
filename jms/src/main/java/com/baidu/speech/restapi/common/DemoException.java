24337c172068516485c4f2e5c8e334dd1541d0ff
Andco <18702149475@163.com>
2020-05-11 23:16:58 +0800

    delete code

--- jms/src/main/java/com/baidu/speech/restapi/common/DemoException.java
+++
@@ -1,7 +0,0 @@
-package com.baidu.speech.restapi.common;
-
-public class DemoException extends Exception {
-    public DemoException(String message) {
-        super(message);
-    }
-}
--- jms/src/main/java/com/ldy/controller/UploadTest.java
+++ jms/src/main/java/com/ldy/controller/UploadTest.java
@@ -1,7 +1,7 @@
 package com.ldy.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.ldy.util.ImageUtil;
+import com.ldy.common.util.ImageUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
