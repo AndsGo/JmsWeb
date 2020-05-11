994a2cb90321efedb21adcd82bb8c47bd65c8c96
AndCoSong <songandco99@gmail.com>
2020-05-11 20:16:04 +0800

    update jms-ui

--- jms-ui/src/main.js
+++ jms-ui/src/main.js
@@ -1,11 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+//引入ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import App from './App.vue'
 
 Vue.use(ElementUI)
+//引入axios,绑定到axios原型链上
+import axios from 'axios'
+Vue.prototype.$axios = axios
+//引入echarts
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  components: { App },
+  template: '<App/>'
 })
---
+++ jms-ui/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Todo from '@/components/todo/todo'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/todo',
+      name: 'Todo',
+      component: Todo
+    }
+  ]
+})
