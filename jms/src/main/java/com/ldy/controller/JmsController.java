7ae6828679949c72f1e059769e9010ad221e8a4f
AndCoSong <songandco99@gmail.com>
2020-05-20 20:26:59 +0800

    增加日志配置

--- jms/src/main/java/com/ldy/controller/JmsController.java
+++ jms/src/main/java/com/ldy/controller/JmsController.java
@@ -3,12 +3,14 @@ package com.ldy.controller;
 import com.ldy.common.response.Response;
 import com.ldy.common.response.ResponseHelper;
 import com.ldy.core.model.Peer;
+import com.ldy.core.model.ServiceDescriptor;
 import com.ldy.core.util.MBeanServerConnectionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Set;
 
 /**
@@ -19,16 +21,21 @@ import java.util.Set;
 public class JmsController {
     private static final Logger logger = LoggerFactory.getLogger(JmsController.class);
     @RequestMapping("/jms/getPeerList")
-    public Response getPeerList(Peer peer) {
+    public Response getPeerList() {
         Set<Peer> peerSet = MBeanServerConnectionUtil.getPeerSet();
         logger.info("peerSet:",peerSet);
         return ResponseHelper.buildOk(peerSet);
     }
 
-    @RequestMapping("/jms/getPeerList2")
-    public Response getPeerList2(Peer peer) {
-        Set<Peer> peerSet = MBeanServerConnectionUtil.getPeerSet();
-        logger.info("peerSet:",peerSet);
-        return ResponseHelper.buildOk(peerSet);
+    @RequestMapping("/jms/getConnection")
+    public Response getConnection(Peer peer) {
+        try {
+            ServiceDescriptor descriptor = MBeanServerConnectionUtil.getMBeanServerConnection(peer);
+            String connectionId = descriptor.getJmxConnector().getConnectionId();
+            return ResponseHelper.buildOk(connectionId);
+        } catch (Exception e) {
+            logger.info("获取连接失败");
+            return ResponseHelper.buildFail(e.getMessage());
+        }
     }
 }
